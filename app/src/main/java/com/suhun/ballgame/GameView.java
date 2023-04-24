8ba813733f45fc9c68a3a0712e1f8d3f89d6407b
suhunlin <lin.suhun@msa.hinet.net>
2023-04-24 14:34:22 +0800

    Define ball width and height scaled to device width / 12

--- app/src/main/java/com/suhun/ballgame/GameView.java
+++ app/src/main/java/com/suhun/ballgame/GameView.java
@@ -14,16 +14,31 @@ public class GameView extends View {
     private String tag = GameView.class.getSimpleName();
     private Resources res;
     private Bitmap bmpBall;
+    private boolean isInitBallInfo;
+    private int ballW, ballH;
+    private float ballX, ballY;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         res = context.getResources();
         setBackgroundResource(R.drawable.bg);
     }
-
+    private void initBallInfo(){
+        float ratio = 12.0f;
+        bmpBall = BitmapFactory.decodeResource(res, R.drawable.ball0);
+        calculateBallScaledRatio(ratio);
+        bmpBall = Bitmap.createScaledBitmap(bmpBall, ballW, ballH, false);
+        isInitBallInfo = true;
+    }
+    private void calculateBallScaledRatio(float ratio){
+        ballW = (int)(getWidth()/ratio);
+        ballH = ballW;
+    }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        bmpBall = BitmapFactory.decodeResource(res, R.drawable.ball0);
-        canvas.drawBitmap(bmpBall, 0, 0, null);
+        if(!isInitBallInfo){
+            initBallInfo();
+        }
+        canvas.drawBitmap(bmpBall, ballX, ballY, null);
     }
 }
