13bc47a9e01afa02919c8148b93e5bd87584ac93
suhunlin <lin.suhun@msa.hinet.net>
2023-04-24 14:23:52 +0800

    Decode bmp file from resource and draw the pic by canvas in position (0, 0)

--- app/src/main/java/com/suhun/ballgame/GameView.java
+++ app/src/main/java/com/suhun/ballgame/GameView.java
@@ -1,14 +1,29 @@
 package com.suhun.ballgame;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
 import androidx.annotation.Nullable;
 
 public class GameView extends View {
+    private String tag = GameView.class.getSimpleName();
+    private Resources res;
+    private Bitmap bmpBall;
     public GameView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        res = context.getResources();
         setBackgroundResource(R.drawable.bg);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        bmpBall = BitmapFactory.decodeResource(res, R.drawable.ball0);
+        canvas.drawBitmap(bmpBall, 0, 0, null);
     }
 }
